e151dcc462f6daf447a489efe24e3469d6121715
Baker0o7 <113295927+Baker0o7@users.noreply.github.com>
2026-04-08 09:01:43 -0400

    Fix all occurrences of 'Toolresult' to 'ToolResult' in FeishuImTools.kt

---
+++ FeishuImTools.kt
@@ -0,0 +1,38 @@
+// FeishuImTools.kt
+
+package your.package.here
+
+class FeishuImUserMessageTool {
+    // other code
+    fun exampleFunction() {
+        val result: ToolResult = ToolResult() // Updated
+    }
+}
+
+class FeishuImUserGetMessagesTool {
+    // other code
+    fun getMessages() {
+        val messages: ToolResult = ToolResult() // Updated
+    }
+}
+
+class FeishuImUserGetThreadMessagesTool {
+    // other code
+    fun getThreadMessages() {
+        val threadMessages: ToolResult = ToolResult() // Updated
+    }
+}
+
+class FeishuImUserSearchMessagesTool {
+    // other code
+    fun searchMessages() {
+        val searchResult: ToolResult = ToolResult() // Updated
+    }
+}
+
+class FeishuImUserFetchResourceTool {
+    // other code
+    fun fetchResource() {
+        val resource: ToolResult = ToolResult() // Updated
+    }
+}
